09a5fb0478b5306100d7594e6d6390a1be885dce
Evalyn Alviar <ealviar.12805@gmail.com>
2026-05-18 23:32:37 +0000

    add loop and homes

--- js/creatures.js
+++ js/creatures.js
@@ -10,7 +10,35 @@ function showHome()
         if creature lives in air, yellow
         
         */
-        alert("showHome()");
+
+    //get all creatures
+    let creatures = document.querySelectorAll(".creature");
+
+    //loop through each creature
+    creatures.forEach(function(el){
+
+        //read data-home attribute
+        let home = el.dataset.home;
+
+        //switch based on home
+        switch(home) {
+            case "land":
+                el.computedStyleMap.backgroundColor = "green";
+            break;
+
+            case "water":
+                el.computedStyleMap.backgroundColor = "blue";
+            break;
+
+            case "air":
+                el.computedStyleMap.backgroundColor = "yellow";
+            break;
+
+            default:
+                console.log("Unknown home:", home);
+        }
+    });
+        // alert("showHome()");
     
 }//end showHome()
 
